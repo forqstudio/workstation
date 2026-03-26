cc086cc4597dc323d9c866b21b82a93d448eeff1
Howard Seraus <howard@forqstudio.nl>
2026-03-26 08:48:48 +0100

    add Night Owl theme extension and set as default color theme

--- run_onchange_install-vscode-extensions.sh
+++ run_onchange_install-vscode-extensions.sh
@@ -7,4 +7,5 @@ if command -v code &>/dev/null; then
   code --install-extension ms-azuretools.vscode-containers
   code --install-extension ms-vscode-remote.remote-containers
   code --install-extension Anthropic.claude-code
+  code --install-extension sdras.night-owl
 fi
