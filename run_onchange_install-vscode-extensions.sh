6e612e42a24f1ac82b86a21cf63bdcf5d8d498da
Howard Seraus <howard@forqstudio.nl>
2026-04-02 15:14:05 +0200

    chore: update vscode user settings

--- run_onchange_install-vscode-extensions.sh
+++ run_onchange_install-vscode-extensions.sh
@@ -5,12 +5,12 @@ set -e
 if command -v code &>/dev/null; then
   # core
   code --install-extension ms-python.python --force
-  code --install-extension ms-dotnettools.csdevkit --force
   code --install-extension ms-azuretools.vscode-containers --force
+  code --install-extension ms-dotnettools.csdevkit --force
   code --install-extension ms-vscode-remote.remote-containers --force
   code --install-extension HashiCorp.terraform --force
-  code --install-extension Anthropic.claude-code --force
   
   # theme
   code --install-extension Catppuccin.catppuccin-vsc --force
+  code --install-extension Catppuccin.catppuccin-vsc-icons --force
 fi
